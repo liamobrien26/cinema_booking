5ee565d4a162df1f4da96961115df3680e036d3c
Liam O'Brien <liamobrien2697@gmail.com>
2024-07-27 22:43:34 +0100

    push new changes

--- src/main/java/com/cinema/booking/controller/UserController.java
+++ src/main/java/com/cinema/booking/controller/UserController.java
@@ -5,17 +5,28 @@ import com.cinema.booking.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@RestController //@RestController tells Maven that the following class is a controller for a web application.
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("/login")
+    @GetMapping("/login") //@GetMapping tells Maven that this code should be used to handle a specific GET request.
     public String login() {
         return "login";
     }
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }//This would just display the text "Greetings from Spring Boot!"
+
+    @GetMapping("/")
+    public ModelAndView home() {
+        return new ModelAndView("home");
+    } //This will land to the home template
 
     @GetMapping("/register")
     public String register() {
--- src/main/java/com/cinema/booking/model/User.java
+++ src/main/java/com/cinema/booking/model/User.java
@@ -27,9 +27,6 @@ public class User {
         this.username = username;
     }
 
-    public String getPassword() {
-        return password;
-    }
 
     public void setPassword(String password) {
         this.password = password;
